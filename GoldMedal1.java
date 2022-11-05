59b584ad6af281776786d22153a3bbefb1629974
kailashrao <kailash7rao@gmail.com>
2022-11-04 18:42:48 -0700

    v1.1.2 - Deleted all uneccesary comments - Updated variable names to be more descriptive - Added descriptive comments throughout code - No functionality change from v1.1.1

--- GoldMedal1.java
+++ GoldMedal1.java
@@ -1,4 +1,3 @@
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -19,100 +18,57 @@ public class GoldMedal1 {
         }
         scan.close();
         int count1 = lines.size();
-        // System.out.println(count1);
 
-        // Initialize lists
+        // Initialize Variables
         List<String> names = new ArrayList<String>(Arrays.asList());
-        List<String> genders = new ArrayList<String>(Arrays.asList());
-        List<String> ids = new ArrayList<String>(Arrays.asList());
         List<String> subjects = new ArrayList<String>(Arrays.asList());
+
         List<Integer> eng_scores = new ArrayList<Integer>(Arrays.asList());
         List<Integer> math_scores = new ArrayList<Integer>(Arrays.asList());
         List<Integer> phy_scores = new ArrayList<Integer>(Arrays.asList());
         List<Integer> chem_scores = new ArrayList<Integer>(Arrays.asList());
         List<Integer> bio_scores = new ArrayList<Integer>(Arrays.asList());
+
         List<List<Integer>> score_list = new ArrayList<List<Integer>>(Arrays.asList(eng_scores, math_scores, phy_scores, chem_scores, bio_scores));
-        // List<Boolean> pass = new ArrayList<Boolean>(Arrays.asList());
-        List<String> toppers1 = new ArrayList<String>();
 
-        int index = 3;
-
-        List<String> temp1 = new ArrayList<String>();
+        // Temporary Use Variables
+        List<String> subject_scores = new ArrayList<String>();
         int max1 = 0;
-
+        List<String> gold_medalists = new ArrayList<String>();
+        int index = 3;
         
         // Parse through each line
         for (String line: lines) {
             // Separate each line at "," to isolate different information
-            List<String> info1 = new ArrayList<String>(Arrays.asList(line.split(",", -1)));
-            names.add(info1.get(0));
-            genders.add(info1.get(1));
-            ids.add(info1.get(2));
+            List<String> line_data = new ArrayList<String>(Arrays.asList(line.split(",", -1)));
+            names.add(line_data.get(0));
 
-            // temp1 = Arrays.asList(info1.get(3).split(":"));
-            // subjects.add(temp1.get(0));
-            // eng_scores.add(Integer.parseInt(temp1.get(1)));
-
-            // temp1 = Arrays.asList(info1.get(4).split(":"));
-            // subjects.add(temp1.get(0));
-            // math_scores.add(Integer.parseInt(temp1.get(1)));
-
+            // Add scores to corresponding subject list
             for (List<Integer> scores: score_list) {
-                temp1 = Arrays.asList(info1.get(index).split(":"));
-                subjects.add(temp1.get(0));
-                scores.add(Integer.parseInt(temp1.get(1)));
+                subject_scores = Arrays.asList(line_data.get(index).split(":"));
+                subjects.add(subject_scores.get(0));
+                scores.add(Integer.parseInt(subject_scores.get(1)));
                 index += 1;
             }
-            index = 3;    
-            // Find all subjects and associated scores
-            // for (String subject: info1.subList(3, info1.size()-1)){
-            //     List<String> subject_score = new ArrayList<String>(Arrays.asList(subject.split(":", -1)));
-            //     if (!subjects.contains(subject_score.get(0))) {
-            //         subjects.add(subject_score.get(0));
-            //     }
-            //     Integer score = Integer.parseInt(subject_score.get(1));
-            //     scores.add(score);
-            // }
-            // Check if student passed or not
-            // if (info1.get(info1.size()-1).equals("PASS")) 
-            //     pass.add(true);
-            // else
-            //     pass.add(false);
-            
+            index = 3;       
         }
+
+        // Find highest score(s) in each subject and corresponding names
         index = 0;
         for (List<Integer> scores: score_list){
             max1 = Collections.max(scores);
-            // System.out.println(max1);
             for (int i = 0; i<count1; i++){
                 if (scores.get(i).equals(max1)){
-                    toppers1.add(names.get(i));
+                    gold_medalists.add(names.get(i));
                 }
             }
-            System.out.println(toppers1 + " are the toppers in " + subjects.get(index) + " with marks of " + max1);
+            System.out.println(gold_medalists + " are the toppers in " + subjects.get(index) + " with marks of " + max1);
             index += 1;
-            toppers1.clear();
+            gold_medalists.clear();
         }
         
-        
-
-
-
-        // Output sorted data
-        // System.out.println(names);
-        // // System.out.println(genders);
-        // // System.out.println(ids);
-        // System.out.println(subjects);
-        // System.out.println(eng_scores);
-        // System.out.println(math_scores);
-        // System.out.println(pass);
-        
-
-
-
 
     }
 
-        
         
 }
